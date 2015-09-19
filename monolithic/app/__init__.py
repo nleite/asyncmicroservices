51a4766b35194a103debd8fe1c4f5d4af4afefa7
Norberto <norberto@10gen.com>
2015-09-19 23:18:42 +0200

    Correction of incorrect MongoSession class: no need to load this class anymore

--- monolithic/app/__init__.py
+++ monolithic/app/__init__.py
@@ -5,10 +5,6 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config.from_object( 'config' )
-app.session_interface = MongoSessionInterface(
-    host=app.config["MONGODB"]["host"],
-    db=app.config["MONGODB"]["session"]["db"])
-
 db = MongoClient(app.config["MONGODB"]["host"])[app.config["MONGODB"]["data"]["db"]]
 fts = 'data'
 from app import views
